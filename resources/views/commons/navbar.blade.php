fa5a4d4e7d177c6fb206446c9dbd176693573753
soya.nakashima <so8.63.nakaso@gmail.com>
2021-08-09 14:52:59 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -21,7 +21,7 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
+                <li class="nav-item">{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
                 {{-- ログインページへのリンク --}}
                 <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
             </ul>

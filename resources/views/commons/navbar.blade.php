dd5f4f7bb9c91b4b89241031da354680e3a273d3
soya.nakashima <so8.63.nakaso@gmail.com>
2021-08-09 14:12:18 +0000

    create nav

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,40 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">DropOffBin</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    
+    </nav>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <ul class="navbar-nav">
+            {{-- 出品用ページへのリンク --}}
+            <li class="nav-item"><a href="#" class="nav-link">出品する</a></li>
+            {{-- マイページへのリンク --}}
+            <li class="nav-item"><a href="#" class="nav-link">マイページ</a></li>
+        </ul>
+    </nav>
+</header>
+
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup','Auth\RegisterController@showRegistertionForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
